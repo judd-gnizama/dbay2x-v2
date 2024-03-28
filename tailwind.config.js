c08053804d4408dfc481c5ff4772ae8762fe8e55
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-03-28 17:33:32 +0800

    removed colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,13 +13,6 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
-    colors: {
-      'text': 'var(--text)',
-      'background': 'var(--background)',
-      'primary': 'var(--primary)',
-      'secondary': 'var(--secondary)',
-      'accent': 'var(--accent)',
-     },
      
      
   },
