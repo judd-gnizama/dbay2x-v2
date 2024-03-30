e7a5226e4140df5b82b2da9864b5fb2bc89e59b1
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-03-30 22:32:36 +0800

    add pages to groups

---
+++ src/app/groups/[groupId]/page.jsx
@@ -0,0 +1,8 @@
+
+export default function GroupPage() {
+  return (
+    <div>
+      Group
+    </div>
+  )
+}
---
+++ src/app/groups/page.jsx
@@ -0,0 +1,8 @@
+
+export default function GroupsPage() {
+  return (
+    <div>
+      Groups
+    </div>
+  )
+}
