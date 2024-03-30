81370856afb114679ba9fda4cede101677486b2f
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-03-30 21:51:35 +0800

    fixed sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,13 +8,13 @@ export default function Header() {
 
   
   return (
-    <div className="flex justify-center gap-4 items-center text-gray-800">
+    <div className="flex justify-center gap-4 items-center text-gray-800 p-4">
       <Brand/>
       <div className="flex items-center justify-end gap-4 w-full max-w-6xl">
         <nav className="flex justify-center items-center gap-4">
-          <Link className='hover:text-teal-700 hover:font-bold' href={'/users'}>Users</Link>
-          <Link className='hover:text-teal-700 hover:font-bold' href={'/transactions'}>Transactions</Link>
-          <Link className='hover:text-teal-700 hover:font-bold' href={'/settlements'}>Settlements</Link>
+          <Link className='hover:text-teal-400' href={'/users'}>Users</Link>
+          <Link className='hover:text-teal-400' href={'/transactions'}>Transactions</Link>
+          <Link className='hover:text-teal-400' href={'/settlements'}>Settlements</Link>
           {/* <Link href='/'>Home</Link>
           <Link href='/about'>About</Link> */}
           <DarkModeSwitch/>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,26 +1,43 @@
 'use client'
+import Link from "next/link";
 import { useState } from "react"
 
-export default function Sidebar({ show }) {
+export default function Sidebar() {
 
   const [ showSidebar , setShowSidebar] = useState(false);
+  const groups = [
+    'Travel to Bohol',
+    'Christmas Vacation at CDO',
+    'Trip to Taiwan', 
+    'Staycation at IT Park'
+  ]
 
   return (
-    <div className="bg-slate-100">
-      <div className="p-4">
-        <span class="material-symbols-outlined size-12 text-center place-content-center cursor-pointer" style={{fontSize: '2rem'}}
+    <div className="bg-slate-100 p-4">
+      <div className="flex flex-col justify-start gap-4">
+        
+        <span className="material-symbols-outlined p-2 w-fit cursor-pointer hover:bg-slate-300 rounded-full" style={{fontSize: '2rem'}}
         onClick={() => showSidebar === false ? setShowSidebar(true) : setShowSidebar(false)}>menu</span>
+        
+        <div className="flex items-center w-fit gap-2 p-2 cursor-pointer hover:bg-slate-300 rounded-full">
+          <span className="material-symbols-outlined "
+          style={{fontSize: '2rem'}}>group_add</span>
+          <p hidden={!showSidebar}>Create Group</p>
+        </div>
+
+        {showSidebar && 
+        <div className="flex flex-col gap-2 pl-2 w-48">
+          <h2 className="font-bold">Available Groups</h2>
+          <div className="grid gap-2">
+            {groups && groups.map(group => 
+              <Link href="/" 
+              className="whitespace-nowrap overflow-hidden text-nowrap text-ellipsis hover:font-bold hover:text-teal-400"
+              >{group}</Link>
+            )}            
+          </div>
+        </div>}
       </div>
       
-      {showSidebar && 
-      <div className="px-4 w-48">
-        <button>Create Group</button>
-        <ul>
-          <li>Group 1</li>
-          <li>Group 2</li>
-          <li>Group 3</li>
-        </ul>
-      </div>}
 
     </div>
   )
