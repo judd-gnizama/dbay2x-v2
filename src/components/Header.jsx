b7b7cef096e9c4cfdc01716dea6d67d121af0c5f
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-03-30 22:58:36 +0800

    added icons to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,10 +8,10 @@ export default function Header() {
 
   
   return (
-    <div className="flex justify-center gap-4 items-center text-gray-800 p-4">
+    <div className="flex justify-center gap-4 items-center p-4">
       <Brand/>
       <div className="flex items-center justify-end gap-4 w-full max-w-6xl">
-        <nav className="flex justify-center items-center gap-4">
+        <nav className="flex justify-center items-center gap-4 max-sm:hidden">
           <Link className='hover:text-teal-400' href={'/users'}>Users</Link>
           <Link className='hover:text-teal-400' href={'/transactions'}>Transactions</Link>
           <Link className='hover:text-teal-400' href={'/settlements'}>Settlements</Link>
@@ -19,6 +19,23 @@ export default function Header() {
           <Link href='/about'>About</Link> */}
           <DarkModeSwitch/>
         </nav>
+        <nav className="flex justify-center items-center gap-4 sm:hidden">
+          <Link className='hover:text-teal-400 flex justify-center items-center' href={'/users'}>
+          <span class="material-symbols-outlined">person</span>
+          </Link>
+          <Link className='hover:text-teal-400 flex justify-center items-center' href={'/transactions'}>
+          <span class="material-symbols-outlined">receipt_long</span>
+          </Link>
+          <Link className='hover:text-teal-400 flex justify-center items-center' href={'/settlements'}>
+          <span class="material-symbols-outlined">handshake</span>
+          </Link>
+          {/* <Link href='/'>Home</Link>
+          <Link href='/about'>About</Link> */}
+          <DarkModeSwitch/>
+        </nav>
+
+        
+
       </div>
     </div>
   )
