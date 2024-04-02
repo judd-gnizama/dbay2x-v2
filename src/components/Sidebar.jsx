f703661840ad1a424127403276f1d0d486c09922
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-04-02 14:41:35 +0800

    hide sidebar links when not in a group

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react"
 import LocalStorageManager from "./LocalStorageManager";
 import { getCurrentGroup, getCurrentGroupId, getGroups, setCurrentGroupId } from "@/functions/LocalStorageFunc";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { createNewGroup } from "@/functions/InterfaceFunc";
 
 export default function Sidebar() {
@@ -13,27 +13,31 @@ export default function Sidebar() {
   const [ _currentGroupId, _setCurrentGroupId ] = useState(0);
   const [ links, setLinks] = useState({});
   const router = useRouter();
+  const pathname = usePathname();
+  console.log(pathname)
+  const showLinks = pathname.startsWith('/groups');
+  console.log(showLinks)
 
   const handleChangeGroup = (groupId) => {
     setCurrentGroupId({groupId: groupId})
     setShowSidebar(false);
+    _setCurrentGroupId(groupId);
+    setLinks({
+      usersLink: `/groups/${groupId}/#users`,
+      transactionsLink: `/groups/${groupId}/#transactions`,
+      settlementsLink: `/groups/${groupId}/#settlements`
+    })
   }
 
   const handleAddNewGroup = () => {
     const { id } = createNewGroup();
     router.push(`/groups/${id}`);
-    _setCurrentGroupId(id);
     handleChangeGroup(id);
   }
 
   useEffect(() => {
   const currentGroupIdFromDb = getCurrentGroupId();
-  setCurrentGroupId(currentGroupIdFromDb);
-  setLinks({
-    usersLink: `/groups/${currentGroupIdFromDb}/#users`,
-    transactionsLink: `/groups/${currentGroupIdFromDb}/#transactions`,
-    settlementsLink: `/groups/${currentGroupIdFromDb}/#settlements`
-  })
+  handleChangeGroup(currentGroupIdFromDb);
   }, [])
 
   useEffect(() => {
@@ -71,22 +75,24 @@ export default function Sidebar() {
             )}            
           </div>
         </div>}
-
-        <Link href={links.usersLink || '/'} className="flex items-center gap-2 p-2 cursor-pointer hover:bg-slate-300 rounded-full z-10">
-          <span className="material-symbols-outlined "
-          style={{fontSize: '2rem'}}>person</span>
-          <p hidden={!showSidebar}>Users</p>
-        </Link>
-        <Link href={links.transactionsLink || '/'} className="flex items-center gap-2 p-2 cursor-pointer hover:bg-slate-300 rounded-full z-10">
-          <span className="material-symbols-outlined "
-          style={{fontSize: '2rem'}}>receipt_long</span>
-          <p hidden={!showSidebar}>Transactions</p>
-        </Link>
-        <Link href={links.settlementsLink || '/'} className="flex items-center gap-2 p-2 cursor-pointer hover:bg-slate-300 rounded-full z-10">
-          <span className="material-symbols-outlined "
-          style={{fontSize: '2rem'}}>handshake</span>
-          <p hidden={!showSidebar}>Settlements</p>
-        </Link>
+        
+        {showLinks && <>
+          <Link href={links.usersLink || '/'} className="flex items-center gap-2 p-2 cursor-pointer hover:bg-slate-300 rounded-full z-10">
+            <span className="material-symbols-outlined "
+            style={{fontSize: '2rem'}}>person</span>
+            <p hidden={!showSidebar}>Users</p>
+          </Link>
+          <Link href={links.transactionsLink || '/'} className="flex items-center gap-2 p-2 cursor-pointer hover:bg-slate-300 rounded-full z-10">
+            <span className="material-symbols-outlined "
+            style={{fontSize: '2rem'}}>receipt_long</span>
+            <p hidden={!showSidebar}>Transactions</p>
+          </Link>
+          <Link href={links.settlementsLink || '/'} className="flex items-center gap-2 p-2 cursor-pointer hover:bg-slate-300 rounded-full z-10">
+            <span className="material-symbols-outlined "
+            style={{fontSize: '2rem'}}>handshake</span>
+            <p hidden={!showSidebar}>Settlements</p>
+          </Link>
+        </>}
 
         {showSidebar && <LocalStorageManager/>}
 
