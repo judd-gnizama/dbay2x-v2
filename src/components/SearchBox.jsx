1d39dab1e264c6d179ce13fb4ac0d3767fbe8b97
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-04-06 19:58:46 +0800

    change filter

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -110,7 +110,7 @@ export default function SearchBox({ type, groupId }) {
     <div className="flex flex-col gap-4">
       <form onSubmit={handleSubmit} className="flex items-center gap-2">
         <div className="relative flex items-center flex-1">
-          <input value={search} onChange={(e) => setSearch(e.target.value)} className="bg-slate-200 dark:bg-gray-500 dark:text-gray-100 p-2 px-4 rounded-full w-full" type="text" placeholder={`Search/Enter New ${type}`} 
+          <input value={search} onChange={(e) => setSearch(e.target.value)} className="bg-slate-200 dark:bg-gray-500 dark:text-gray-100 p-2 px-4 rounded-full w-full" type="text" placeholder={`Filter/Add New`} 
           />
           <span 
           className="material-symbols-outlined text-slate-600 absolute right-2 rounded-full cursor-pointer hover:bg-slate-300"
