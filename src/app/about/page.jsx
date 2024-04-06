786ebf618b411366ed501118fc2dc56234125ece
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-04-07 01:39:07 +0800

    used correct HTML code for quotes

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,13 +3,13 @@ export default function AboutPage() {
     <div className="flex flex-col justify-center max-w-4xl gap-4 justify-self-center">
       <h1 className="font-bold text-4xl">About</h1>
       <p className="text-lg">
-        "Dbay2x" is a reference to the visayan kid's way of saying <strong className="font-bold">"divided by"</strong>. Well, at least it was when I was a kid in the late 90s. Hence, the name for a bill-splitting web app. 
+      &#34;Dbay2x&#34; is a reference to the visayan kid&#39;s way of saying <strong className="font-bold">&#34;divided by&#34;</strong>. Well, at least it was when I was a kid in the late 90s. Hence, the name for a bill-splitting web app. 
         <br /><br />
         Dbay2x is a web app that aims to simply the process of dividing expenses among friends and calculating the transactions needed for settling balances and debts. 
         <br /><br />
         This is also a mini-project that I wanted to create as a way of practicing my new skills in building web apps using NextJS, TailwindCSS and others. 
         <br /><br />
-        If you want to see other projects I've done, you can <a className="font-bold text-teal-400" href="https://portfolio-be893.web.app/" target="_blank" rel="noopener noreferrer">click here</a>.
+        If you want to see other projects I&#39;ve done, you can <a className="font-bold text-teal-400" href="https://portfolio-be893.web.app/" target="_blank" rel="noopener noreferrer">click here</a>.
         
         </p>
     </div>
