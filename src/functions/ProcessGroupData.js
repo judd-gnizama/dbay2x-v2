88955332f57eb6e435c3dc00ab6737fb962a64bd
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-04-07 16:23:17 +0800

    changes processgroup

--- src/functions/ProcessGroupData.js
+++ src/functions/ProcessGroupData.js
@@ -1,147 +1,147 @@
 
-export function ProcessGroupData(group) {
+// export function ProcessGroupData(group) {
   
-  const initializeMembers = (membersList) => {
-    if (!membersList || !membersList.length) return []
+//   const initializeMembers = (membersList) => {
+//     if (!membersList || !membersList.length) return []
 
-    const newMembersList = membersList.map(member => ({
-      id: member.id,
-      name: member.name,
-      paid: 0,
-      share: 0,
-      net: 0
-    }))
-    return newMembersList
-  }
+//     const newMembersList = membersList.map(member => ({
+//       id: member.id,
+//       name: member.name,
+//       paid: 0,
+//       share: 0,
+//       net: 0
+//     }))
+//     return newMembersList
+//   }
 
-  const appendMembersNet = (membersList) => {
-    return membersList.map((member) => ({...member, net: member.share - member.paid}))
-  }
+//   const appendMembersNet = (membersList) => {
+//     return membersList.map((member) => ({...member, net: member.share - member.paid}))
+//   }
 
-  const computeMembersList = (transactions) => {
+//   const computeMembersList = (transactions) => {
 
-    if (!transactions || !transactions.length) return []; //Early return empty
+//     if (!transactions || !transactions.length) return []; //Early return empty
 
-    var deltaMemberList = []
+//     var deltaMemberList = []
 
-    transactions.forEach((transaction) => {
+//     transactions.forEach((transaction) => {
 
-      const { payer, type, amount, split_members, recipient } = transaction
+//       const { payer, type, amount, split_members, recipient } = transaction
       
-      let memberIndex = deltaMemberList.findIndex((item) => item.id === payer)
+//       let memberIndex = deltaMemberList.findIndex((item) => item.id === payer)
       
-      if(memberIndex !== -1) {
-        deltaMemberList[memberIndex].paid += amount;
-      } else {
-        deltaMemberList.push(
-            {
-              id: payer,
-              paid: amount, 
-              share: 0,
-            }
-          );
-      }       
-      if (type === 1 && split_members && split_members.length) {
-        // solve total shares
-        let total_shares = 0;
-        split_members.forEach((item)=>total_shares += item.share)
-        split_members.forEach((split_member) => {
-          // find if split_member is already present
-          let memberIndex = deltaMemberList.findIndex((item)=>item.id === split_member.id)
-          if(memberIndex !== -1) {
-            deltaMemberList[memberIndex].share += split_member.share / total_shares * amount
+//       if(memberIndex !== -1) {
+//         deltaMemberList[memberIndex].paid += amount;
+//       } else {
+//         deltaMemberList.push(
+//             {
+//               id: payer,
+//               paid: amount, 
+//               share: 0,
+//             }
+//           );
+//       }       
+//       if (type === 1 && split_members && split_members.length) {
+//         // solve total shares
+//         let total_shares = 0;
+//         split_members.forEach((item)=>total_shares += item.share)
+//         split_members.forEach((split_member) => {
+//           // find if split_member is already present
+//           let memberIndex = deltaMemberList.findIndex((item)=>item.id === split_member.id)
+//           if(memberIndex !== -1) {
+//             deltaMemberList[memberIndex].share += split_member.share / total_shares * amount
 
-          } else {
-            deltaMemberList.push({
-              id: split_member.id,
-              paid: 0,
-              share: split_member.share / total_shares * amount,
-            });
-          }
-        });
-      } else if (type === 2) {
-        let memberIndex = deltaMemberList.findIndex((item) => item.id === recipient)
+//           } else {
+//             deltaMemberList.push({
+//               id: split_member.id,
+//               paid: 0,
+//               share: split_member.share / total_shares * amount,
+//             });
+//           }
+//         });
+//       } else if (type === 2) {
+//         let memberIndex = deltaMemberList.findIndex((item) => item.id === recipient)
         
-        if(memberIndex !== -1) {
-          deltaMemberList[memberIndex].paid -= amount;
-        } else {
-          deltaMemberList.push(
-              {
-                id: recipient,
-                paid: -amount, 
-                share: 0,
-              }
-            );
-        }    
-      }
-    });
-    return appendMembersNet(deltaMemberList);
-  }
+//         if(memberIndex !== -1) {
+//           deltaMemberList[memberIndex].paid -= amount;
+//         } else {
+//           deltaMemberList.push(
+//               {
+//                 id: recipient,
+//                 paid: -amount, 
+//                 share: 0,
+//               }
+//             );
+//         }    
+//       }
+//     });
+//     return appendMembersNet(deltaMemberList);
+//   }
 
-  const computeReimbursements = (membersList) => {
-    if (!membersList || !membersList.length) return [];
+//   const computeReimbursements = (membersList) => {
+//     if (!membersList || !membersList.length) return [];
 
-    let reimbursements = [];
-    let done = false;
-    let count = 0;
-    while (!done) {
+//     let reimbursements = [];
+//     let done = false;
+//     let count = 0;
+//     while (!done) {
 
 
-      if (membersList.findIndex((member) => member.net > 0) === -1 || count === 100) {
-        done = true;
-        break;
-      }
-      // sort lowest to highest
-      let sortedMembersList = membersList.sort((member1, member2) => member1.net - member2.net);
+//       if (membersList.findIndex((member) => member.net > 0) === -1 || count === 100) {
+//         done = true;
+//         break;
+//       }
+//       // sort lowest to highest
+//       let sortedMembersList = membersList.sort((member1, member2) => member1.net - member2.net);
       
       
-      // append to reimbursements
-      const maxIndex = sortedMembersList.length - 1;
-      reimbursements.push(
-        {
-          from: sortedMembersList[maxIndex].id,
-          to: sortedMembersList[0].id,
-          amount: sortedMembersList[maxIndex].net
-        }
-        )
+//       // append to reimbursements
+//       const maxIndex = sortedMembersList.length - 1;
+//       reimbursements.push(
+//         {
+//           from: sortedMembersList[maxIndex].id,
+//           to: sortedMembersList[0].id,
+//           amount: sortedMembersList[maxIndex].net
+//         }
+//         )
         
-        // update net list
-        sortedMembersList[0].net += sortedMembersList[maxIndex].net;
-        sortedMembersList[maxIndex].net = 0;
-      }
+//         // update net list
+//         sortedMembersList[0].net += sortedMembersList[maxIndex].net;
+//         sortedMembersList[maxIndex].net = 0;
+//       }
       
-    return reimbursements;
+//     return reimbursements;
 
-  }
+//   }
 
-  const mergeMembersList = (oldMembersList, computedMembersList) => {
-    computedMembersList.forEach((newMember) => {
-      const memberIndex = oldMembersList.findIndex(item => item.id === newMember.id)
-      if (memberIndex !== -1) {
-        oldMembersList[memberIndex] = {... oldMembersList[memberIndex], paid: newMember.paid, share: newMember.share };
-      }
-    })
+//   const mergeMembersList = (oldMembersList, computedMembersList) => {
+//     computedMembersList.forEach((newMember) => {
+//       const memberIndex = oldMembersList.findIndex(item => item.id === newMember.id)
+//       if (memberIndex !== -1) {
+//         oldMembersList[memberIndex] = {... oldMembersList[memberIndex], paid: newMember.paid, share: newMember.share };
+//       }
+//     })
       
-    return oldMembersList;
-  }
+//     return oldMembersList;
+//   }
 
   
-  const { users, transactions} = group;
+//   const { users, transactions} = group;
     
-  const initializedUsers = initializeMembers(users);
-  const involvedMembersList = computeMembersList(transactions);
-  const mergedMembersList = mergeMembersList(initializedUsers, involvedMembersList);
-  const newMembers = appendMembersNet(mergedMembersList);
-  const reimbursements = computeReimbursements(newMembers);
-  const _newMembers = appendMembersNet(mergedMembersList);
-  const updatedGroup = {...group,
-    transactions: transactions,
-    users: _newMembers,
-    reimbursements: reimbursements,
-  }
-  return updatedGroup
+//   const initializedUsers = initializeMembers(users);
+//   const involvedMembersList = computeMembersList(transactions);
+//   const mergedMembersList = mergeMembersList(initializedUsers, involvedMembersList);
+//   const newMembers = appendMembersNet(mergedMembersList);
+//   const reimbursements = computeReimbursements(newMembers);
+//   const _newMembers = appendMembersNet(mergedMembersList);
+//   const updatedGroup = {...group,
+//     transactions: transactions,
+//     users: _newMembers,
+//     reimbursements: reimbursements,
+//   }
+//   return updatedGroup
 
-}
+// }
 
 
 export function processGroup(group) {
@@ -168,6 +168,7 @@ export function processGroup(group) {
       const total_weight = computeTotalWeight(transaction);
 
       paid_expense += transaction.payer === user.id && transaction.type === 'Expense' ? transaction.amount : 0
+      
       paid_settlement += transaction.payer === user.id && transaction.type === 'Settlement' ? transaction.amount : 0
       received += transaction.recipient === user.id && transaction.type === 'Settlement' ? transaction.amount : 0
       transaction.split_members.forEach(split_member => {
@@ -187,46 +188,54 @@ export function processGroup(group) {
       _net: share - paid_expense - paid_settlement + received
     })
   })
-
   const computeReimbursements = (membersList) => {
     if (!membersList || !membersList.length) return [];
-
+    
     let reimbursements = [];
     let done = false;
     let count = 0;
+    
+    
     while (!done) {
-
-
+      
       if (membersList.findIndex((member) => member._net > 0) === -1 || count === 100) {
         done = true;
-        console.log('broke')
+        // console.log('broke')
         break;
       }
+      
       // sort lowest to highest
-      let sortedMembersList = membersList.sort((member1, member2) => member1._net - member2._net);
+      let sortedMembersList = membersList.sort((member1, member2) => member1._net - member2._net);  // (-) -> to receive, (+) -> to pay
+      
+      console.log(`iteration: ${count}`,sortedMembersList)
       
       
       // append to reimbursements
       const maxIndex = sortedMembersList.length - 1;
+
+      const minValue = sortedMembersList[0]._net;
+      const maxValue = sortedMembersList[maxIndex]._net;
+      
       reimbursements.push(
         {
           from: sortedMembersList[maxIndex].id,
           to: sortedMembersList[0].id,
-          amount: sortedMembersList[maxIndex]._net
+          amount: Math.abs(minValue + maxValue)
         }
         )
-        
         // update net list
-        sortedMembersList[0]._net += sortedMembersList[maxIndex]._net;
-        sortedMembersList[maxIndex]._net = 0;
+        sortedMembersList[0] = {...sortedMembersList[0], _net: minValue + maxValue < 0 ? minValue + maxValue : 0}
+        sortedMembersList[maxIndex] = {...sortedMembersList[maxIndex], _net: minValue + maxValue < 0 ? 0 : minValue + maxValue}
+        count += 1
       }
       
-    return reimbursements;
-
+      return reimbursements;
+    }
+    
+    
+    console.log(updatedUsers)
+    const reimbursements = computeReimbursements(updatedUsers);
+    
+    return {...group, users: updatedUsers, reimbursements: reimbursements}
+    
   }
-
-  const reimbursements = computeReimbursements(updatedUsers);
-
-  return {...group, users: updatedUsers, reimbursements: reimbursements}
-
-}
